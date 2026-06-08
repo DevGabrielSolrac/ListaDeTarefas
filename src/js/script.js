71231213949fc8fcf256bceb160c2a84f209e8d4
Gabriel Carlos <gabrielsolrac.contato@gmail.com>
2026-06-08 17:27:51 -0300

    Adiciona funcionalidade de ter concluido uma tarefa ao clicar no botão concluido e junto a funcionalidade de desfazer, alternando os botões (Concluir/Desfazer, mas o CSS para identificar que foi alterado a tarefa para concluido

--- src/js/script.js
+++ src/js/script.js
@@ -4,17 +4,21 @@ const listTarefas = document.getElementById("list");
 
 btnAdd.addEventListener("click", () => {
     const campoText = cmpTextTarefas.value; //Pegando o texto digitado
-    const addItemList = document.createElement("li"); //Criando o elemento lista.
+    const addItemList = document.createElement("li"); //Criando o elemento <li>.
     const removeList = document.createElement("button"); //Criando o elemento botão para remover item da lista.
+    const concluidoList = document.createElement("button"); //Criando o elemento botão para concluir item da lista.
 
     //Aqui vai verificar se barra de texto está em branco, caso esteje vai não irá fazer nada e não irá adicionar na lista.
     if(campoText.trim() != ""){
         
         addItemList.textContent = campoText + " ";// Define o texto da tarefa dentro do <li>.
         removeList.textContent = "Excluir";// Define o texto que aparecerá no botão.
+        concluidoList.textContent = "Concluir"; // Define o texto que aparecerá no botão.
+        
 
         listTarefas.appendChild(addItemList);// Aqui irá add um item na lista.
-        addItemList.appendChild(removeList);    // Adiciona o botão dentro do <li>.
+        addItemList.appendChild(removeList);// Adiciona o botão dentro do <li> de excluir.
+        addItemList.appendChild(concluidoList) // Adiciona o botão dentro do <li> de concluir.
 
         cmpTextTarefas.value = "";//Aqui irá apagar o que foi escrito.
 
@@ -22,5 +26,18 @@ btnAdd.addEventListener("click", () => {
         removeList.addEventListener("click", () => {
             addItemList.remove();
         });
+
+        //Ação do botão concluir uma tarefa.
+        concluidoList.addEventListener("click", () => {
+
+            addItemList.classList.toggle("concluido");
+            
+            //Condição de estar alternando os botões.
+            if(addItemList.classList.contains("concluido")){
+                concluidoList.textContent = "Desfazer"; //Se estiver concluido.
+            }else{
+                concluidoList.textContent = "Concluir"; //Não estiver concluido.
+            }
+        })
     }
 });
