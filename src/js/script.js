dc068cb394e8432e32d98b8cd330bc751808c741
Gabriel Carlos <gabrielsolrac.contato@gmail.com>
2026-06-08 20:25:15 -0300

    Adicionei funcionalidades para criar/salvar/carregar a lista de tarefas para que toda vez que abrir a pagina ficar salvo a lista de tarefas, para não precisar fazer tudo denovo toda vez que entrar.

--- src/js/script.js
+++ src/js/script.js
@@ -15,43 +15,88 @@ cmpTextTarefas.addEventListener("keydown",(event) => {
 });
 
 
+// Função responsável por adicionar uma nova tarefa.
+// Esta função faz: Pega o texto → Verifica se está vazio → Cria a tarefa → Salva → Limpa o campo.
 function functionsTarefas(){
     const campoText = cmpTextTarefas.value; //Pegando o texto digitado
+
+    if (campoText.trim() != ""){
+
+        criarTarefa(campoText);
+        saveList(campoText);
+
+        cmpTextTarefas.value = "";
+        
+    }
+}
+
+
+//Função para salvar a lista de tarefas
+function saveList(tarefa){
+
+    const carregarLista = JSON.parse(localStorage.getItem("tarefa")) || [];//Carregando a lista salva
+
+    carregarLista.push(tarefa);//Adicionando uma nova tarefa
+    
+
+    localStorage.setItem("tarefa",JSON.stringify(carregarLista));//Salvando novamente as tarefas.
+}
+
+
+//Função para carregar a lista de tarefas.
+function carregarListaTarefas(){
+    
+    const listTarefa = JSON.parse(localStorage.getItem("tarefa")) || [];//Carregando a lista
+    
+    listTarefa.forEach((tarefa) => {
+        criarTarefa(tarefa);   
+    });
+}
+
+
+//Aqui vai ser as funções de criação de tarefas como add/remover/desfazer.
+function criarTarefa(texto) {
     const addItemList = document.createElement("li"); //Criando o elemento <li>.
     const removeList = document.createElement("button"); //Criando o elemento botão para remover item da lista.
     const concluidoList = document.createElement("button"); //Criando o elemento botão para concluir item da lista.
 
-    //Aqui vai verificar se barra de texto está em branco, caso esteje vai não irá fazer nada e não irá adicionar na lista.
-    if(campoText.trim() != ""){
+    addItemList.textContent = texto + " ";// Define o texto da tarefa dentro do <li>.
+    removeList.textContent = "Excluir";// Define o texto que aparecerá no botão.
+    concluidoList.textContent = "Concluir"; // Define o texto que aparecerá no botão.
+    
+
+    listTarefas.appendChild(addItemList);// Aqui irá add um item na lista.
+    addItemList.appendChild(removeList);// Adiciona o botão dentro do <li> de excluir.
+    addItemList.appendChild(concluidoList); // Adiciona o botão dentro do <li> de concluir.
+
+    //Ação do botão remover.
+    removeList.addEventListener("click", () => {
+        //Vai remover da lista de tarefas.
+        addItemList.remove();
         
-        addItemList.textContent = campoText + " ";// Define o texto da tarefa dentro do <li>.
-        removeList.textContent = "Excluir";// Define o texto que aparecerá no botão.
-        concluidoList.textContent = "Concluir"; // Define o texto que aparecerá no botão.
+        //Aqui vai remover da memoria, com isso quando abrir a pagina novamente não vai aparecer mais o que foi salvo.
+        const lista = JSON.parse(localStorage.getItem("tarefa")) || [];
+        const indice = lista.indexOf(texto);
+
+        lista.splice(indice,1);
         
+        localStorage.setItem("tarefa",JSON.stringify(lista));
+    });
 
-        listTarefas.appendChild(addItemList);// Aqui irá add um item na lista.
-        addItemList.appendChild(removeList);// Adiciona o botão dentro do <li> de excluir.
-        addItemList.appendChild(concluidoList) // Adiciona o botão dentro do <li> de concluir.
+    //Ação do botão concluir uma tarefa.
+    concluidoList.addEventListener("click", () => {
 
-        cmpTextTarefas.value = "";//Aqui irá apagar o que foi escrito.
-
-        //Ação do botão remover.
-        removeList.addEventListener("click", () => {
-            addItemList.remove();
-        });
-
-        //Ação do botão concluir uma tarefa.
-        concluidoList.addEventListener("click", () => {
-
-            addItemList.classList.toggle("concluido");
-            
-            //Condição de estar alternando os botões.
-            if(addItemList.classList.contains("concluido")){
-                concluidoList.textContent = "Desfazer"; //Se estiver concluido.
-            }else{
-                concluidoList.textContent = "Concluir"; //Não estiver concluido.
-            }
-        })
-    }
+        addItemList.classList.toggle("concluido");
+        
+        //Condição de estar alternando os botões.
+        if(addItemList.classList.contains("concluido")){
+            concluidoList.textContent = "Desfazer"; //Se estiver concluido.
+        }else{
+            concluidoList.textContent = "Concluir"; //Não estiver concluido.
+        }
+    })
 }
 
+
+//carregando a lista
+carregarListaTarefas();
