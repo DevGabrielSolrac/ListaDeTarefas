d2f6f705412299ad04829d8263f08e7211aadb8c
Gabriel Carlos <gabrielsolrac.contato@gmail.com>
2026-06-08 16:42:00 -0300

    Adiciona funcionalidade de criação de tarefas ao clicar no botão Adicionar

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+const cmpTextTarefas = document.getElementById("tarefas");
+const btnAdd = document.getElementById("adicionar");
+const listTarefas = document.getElementById("list");
+
+btnAdd.addEventListener("click", () => {
+    const campoText = cmpTextTarefas.value; //Pegando o texto digitado
+    const addItemList = document.createElement("li"); //Criando o elemento lista.
+    const removeList = document.createElement("button"); //Criando o elemento botão para remover item da lista.
+
+    //Aqui vai verificar se barra de texto está em branco, caso esteje vai não irá fazer nada e não irá adicionar na lista.
+    if(campoText.trim() != ""){
+        
+        addItemList.textContent = campoText + " ";// Define o texto da tarefa dentro do <li>.
+
+        listTarefas.appendChild(addItemList);   // Aqui irá add um item na lista.
+
+        cmpTextTarefas.value = "";      //Aqui irá apagar o que foi escrito.
+    }
+});
